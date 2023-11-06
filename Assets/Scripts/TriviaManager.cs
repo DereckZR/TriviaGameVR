7f954b0bfbaddc5f32c611e0f9b0db6b69209fe3
DereckZR <dereckzeballos@gmail.com>
2023-11-06 16:32:11 -0400

    Mecanica trivia arreglada

--- Assets/Scripts/TriviaManager.cs
+++ Assets/Scripts/TriviaManager.cs
@@ -5,36 +5,54 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Collections;
 using UnityEngine.UI;
+using Unity.VisualScripting;
 
 public class TriviaManager : MonoBehaviour
 {
+    [SerializeField]
+    TextAsset TXTjson;
+
+    [System.Serializable]
+    public class QuestionList
+    {
+        public TriviaQuestion[] questions;
+    }
+
+    public QuestionList myQuestionList = new QuestionList();
+
+    private void Start() 
+    {
+        myQuestionList = JsonUtility.FromJson<QuestionList>(TXTjson.text);
+        GetNewQuestion();
+    }
+
     [SerializeField] TextMeshProUGUI questionUI;
     [SerializeField] GameObject[] buttonsUI;
     [SerializeField] TextMeshProUGUI[] answersUI;
     private int index;
     [SerializeField] float delay = 2.0f;
-    List<TriviaQuestion> questions = new List<TriviaQuestion>();
+    //List<TriviaQuestion> questions = new List<TriviaQuestion>();
 
-    private void Start() 
+    /*private void Start() 
     {
         questions = FileHandler.ReadFromJSON<TriviaQuestion>("QuestionsDataFile.json");
         GetNewQuestion();
-    }
+    }*/
 
     private void GetNewQuestion()
     {
         System.Random random = new System.Random();
-        index = random.Next(0, questions.Count);
-        questionUI.text = questions[index].question;
-        answersUI[0].text = questions[index].options[0];
-        answersUI[1].text = questions[index].options[1];
-        answersUI[2].text = questions[index].options[2];
-        answersUI[3].text = questions[index].options[3];
+        index = random.Next(0, myQuestionList.questions.Length);
+        questionUI.text = myQuestionList.questions[index].question;
+        answersUI[0].text = myQuestionList.questions[index].options[0];
+        answersUI[1].text = myQuestionList.questions[index].options[1];
+        answersUI[2].text = myQuestionList.questions[index].options[2];
+        answersUI[3].text = myQuestionList.questions[index].options[3];
     }
 
     public void GetResponse(int indexResponse)
     {
-        if(questions[index].correctOptionIndex == indexResponse)
+        if(myQuestionList.questions[index].correctOptionIndex == indexResponse)
         {
             Debug.Log("correct");
             StartCoroutine(DelayedGetNewQuestion(Color.green, indexResponse));
